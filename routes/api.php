3052e7dd3e521e3213813144e61c27023dfc1488
Ramiruuu <oclaritremar25@gmail.com>
2026-05-13 16:41:04 +0800

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\WorkoutController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::get('/workouts', [WorkoutController::class, 'index']);
+    Route::post('/workouts/run', [WorkoutController::class, 'storeRun']);
+    Route::post('/workouts/gym', [WorkoutController::class, 'storeGym']);
+    Route::put('/workouts/{id}', [WorkoutController::class, 'update']);
+    Route::delete('/workouts/{id}', [WorkoutController::class, 'destroy']);
+    
+    Route::get('/comparison', [WorkoutController::class, 'getCalorieComparison']);
+});
